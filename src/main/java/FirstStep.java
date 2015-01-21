40fb8abcbc1378e77afafb2082c125e4b6394ecd
lilinhan <lilinhan1303@gmail.com>
2015-01-21 16:07:09 +0800

    problem

---
+++ src/main/java/FirstStep.java
@@ -0,0 +1,24 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+/**
+ * Created by lewin on 1/21/15.
+ */
+public class FirstStep {
+    public static void main(String[] args)throws IOException{
+            Document doc = Jsoup.connect("http://news.163.com/domestic/").get();
+            getUrlAndKeeo Get = new getUrlAndKeeo();
+            Elements str = doc.select("h2");
+            for(Element s : str) {
+                Elements a = s.select("a[href]");
+                String link = a.attr("href");
+                if (link.contains("news") && link.contains("#")) {
+                    Get.GetKeep(link);
+                }
+            }
+    }
+}
--- src/main/java/SimpleWebSpider.java
+++ src/main/java/SimpleWebSpider.java
@@ -1,8 +1,6 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
-import javax.lang.model.element.Element;
 
 /**
  * Created by lewin on 1/15/15.
@@ -10,16 +8,39 @@ import javax.lang.model.element.Element;
 public class SimpleWebSpider {
     public static void main(String[] s) {
         try {
-            Document doc = Jsoup.connect("http://news.163.com/smart/0/3/3014/1.html").get();
+            Document doc = Jsoup.connect("http://news.163.com/15/0121/08/AGFJIEVV00014JB6.html").get();
 //            Elements str = doc.getElementsByClass("header-info");
 //            String ss = str.text();
 //            System.out.println(ss);
-            Elements str = doc.getElementsByClass("header-digest");
-            for(org.jsoup.nodes.Element tag : str)  {
-                System.out.println(tag.text());
-            }
+//            Elements str = doc.getElementsByClass("item-digest");
+//            for(org.jsoup.nodes.Element tag : str)  {
+//                System.out.println(tag.text());
+//            }
 //            String ss = str.text();
 //            System.out.println(ss);
+//            org.jsoup.nodes.Element ele = doc.getElementById("endText");
+//            String str = ele.text();
+//            System.out.println(str);
+            //图片网址
+//            org.jsoup.nodes.Element ele = doc.getElementById("endText");
+//            Elements eles = ele.select("img[src]");
+//            String imgUrl = eles.attr("abs:src");
+//            if(imgUrl.contains("jpg"))
+//                System.out.println(imgUrl);
+//
+//            Elements str = doc.getElementsByClass("ep-time-soure");
+//            String ss = str.text();
+//            String newData = ss.substring(0,ss.indexOf(" "));
+//            System.out.print(newData);
+//            System.out.println("aaaa");
+//              标题
+//            Element str = doc.getElementById("h1title");
+//            String ss = str.text();
+//            System.out.println(ss);
+            //文本
+            org.jsoup.nodes.Element E = doc.getElementById("endText");
+            String st = E.text();
+            System.out.println(st);
         }catch (Exception e)  {
             e.printStackTrace();
         }
